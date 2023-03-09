d67b80eff785970c926c4f66b16c654b3ec1455d
Ziqian Liao <liaoziqian101@outlook.com>
2023-03-08 22:18:50 -0500

    edited to fix the bug

--- pages/1_Stratified_Chronic_Disease_Mortality_Rates.py
+++ pages/1_Stratified_Chronic_Disease_Mortality_Rates.py
@@ -36,8 +36,6 @@ with st.sidebar:
     disease = st.selectbox('Disease', df2['Question'].unique())
     subdata = subdata[subdata['Question'] == disease]
 
-domain_min = df2['Rate'].loc[df2['Question'] == disease].loc[df2['Stratification1'] == strat].loc[pd.notna(df2['Rate'])].apply(float).min()
-domain_max = df2['Rate'].loc[df2['Question'] == disease].loc[df2['Stratification1'] == strat].loc[pd.notna(df2['Rate'])].apply(float).max()
 
 width = 600
 height  = 400
@@ -54,57 +52,64 @@ background = alt.Chart(states
 
 selector = alt.selection_multi(on = 'click', fields = ['State'], empty = 'all')
 
-rate_scale = alt.Scale(domain=(domain_min, domain_max), scheme='oranges')
-rate_color = alt.Color(field="Rate", type="quantitative", scale=rate_scale)
 
-chart_rate = alt.Chart(states).mark_geoshape().encode(
-    color = rate_color,
-    opacity = alt.condition(selector, alt.value(1), alt.value(0)),
-    tooltip = ['State:N','Rate:Q']
-    ).transform_lookup(
-        lookup="id",
-        from_=alt.LookupData(subdata, "LocationID", ["Rate", 'State', 'Question', 'YearStart', 'Stratification1']),
-    ).project(
-     type='albersUsa'
-    ).add_selection(selector
-    ).transform_filter(selector
-    ).properties(
-        width=width,
-        height=height,
-        title=f'Age-adjusted Mortality Rate of {disease}'
-        )
-
-df2_sub = df2.loc[df2['Question'] == disease].loc[df2['Stratification1'] == strat]
-df2_sub_avg = df2_sub.groupby(['YearStart','State'])['Rate'].mean().reset_index().groupby('YearStart')['Rate'].mean().reset_index()
-
-trendline = alt.Chart(df2_sub).properties(
-    width = width,
-    height = height/2
-).add_selection(
-    selector
-).transform_filter(
-    selector
-).mark_line(point = True
-).encode(
-    x='Year:T',
-    y=alt.Y('Rate', type="quantitative", scale=alt.Scale(domain=[df2_sub['Rate'].loc[pd.notna(df2['Rate'])].apply(float).min(), df2_sub['Rate'].loc[pd.notna(df2['Rate'])].apply(float).max()])),
-    color=alt.Color(field="State", type="nominal", scale=alt.Scale(scheme = 'oranges'), legend=alt.Legend(symbolLimit=10)),
-)
-
-allstates = df2['LocationDesc'].unique().tolist()
-allstates.remove('United States')
-allstates.sort()
-
-comparestates = st.multiselect(
-    'Select up to 5 states to keep track of and compare their statistics',
-    allstates,
-    ['Massachusetts','West Virginia', 'Texas', 'California','New York'], max_selections = 5)
-
-col_all = st.columns(5)
 
 if len(subdata) == 0:
     st.write("No data avaiable for given subset.")
 else:
+    domain_min = df2['Rate'].loc[df2['Question'] == disease].loc[df2['Stratification1'] == strat].loc[pd.notna(df2['Rate'])].apply(float).min()
+    domain_max = df2['Rate'].loc[df2['Question'] == disease].loc[df2['Stratification1'] == strat].loc[pd.notna(df2['Rate'])].apply(float).max()
+    if pd.notnull(domain_min) and pd.notnull(domain_max):
+        rate_scale = alt.Scale(domain=(domain_min, domain_max), scheme='oranges')
+    else:
+        rate_scale = alt.Scale(scheme='oranges')
+    rate_color = alt.Color(field="Rate", type="quantitative", scale=rate_scale)
+
+    chart_rate = alt.Chart(states).mark_geoshape().encode(
+        color = rate_color,
+        opacity = alt.condition(selector, alt.value(1), alt.value(0)),
+        tooltip = ['State:N','Rate:Q']
+        ).transform_lookup(
+            lookup="id",
+            from_=alt.LookupData(subdata, "LocationID", ["Rate", 'State', 'Question', 'YearStart', 'Stratification1']),
+        ).project(
+        type='albersUsa'
+        ).add_selection(selector
+        ).transform_filter(selector
+        ).properties(
+            width=width,
+            height=height,
+            title=f'Age-adjusted Mortality Rate of {disease}'
+            )
+
+    df2_sub = df2.loc[df2['Question'] == disease].loc[df2['Stratification1'] == strat]
+    df2_sub_avg = df2_sub.groupby(['YearStart','State'])['Rate'].mean().reset_index().groupby('YearStart')['Rate'].mean().reset_index()
+
+    trendline = alt.Chart(df2_sub).properties(
+        width = width,
+        height = height/2
+    ).add_selection(
+        selector
+    ).transform_filter(
+        selector
+    ).mark_line(point = True
+    ).encode(
+        x='Year:T',
+        y=alt.Y('Rate', type="quantitative", scale=alt.Scale(domain=[df2_sub['Rate'].loc[pd.notna(df2['Rate'])].apply(float).min(), df2_sub['Rate'].loc[pd.notna(df2['Rate'])].apply(float).max()])),
+        color=alt.Color(field="State", type="nominal", scale=alt.Scale(scheme = 'oranges'), legend=alt.Legend(symbolLimit=10)),
+    )
+
+    allstates = df2['LocationDesc'].unique().tolist()
+    allstates.remove('United States')
+    allstates.sort()
+
+    comparestates = st.multiselect(
+        'Select up to 5 states to keep track of and compare their statistics',
+        allstates,
+        ['Massachusetts','West Virginia', 'Texas', 'California','New York'], max_selections = 5)
+
+    col_all = st.columns(5)
+
     st.altair_chart(alt.vconcat(background + chart_rate, trendline).resolve_scale(color='shared'), use_container_width=True)
     for i in range(len(comparestates)):
         val = subdata.loc[subdata['LocationDesc'] == comparestates[i]]['Rate']
